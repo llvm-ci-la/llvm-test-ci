56a24b1f19f04e53d816f3f820d12a5571dc7358
tangaac <tangyan01@loongson.cn>
2025-08-26 08:52:58 +0800

    enable compiler-rt

--- scripts/build-llvm-release.sh
+++ scripts/build-llvm-release.sh
@@ -12,6 +12,7 @@ cmake $LLVM_SOURCE_DIR/llvm \
   -DBUILD_SHARED_LIBS=true \
   -DLLVM_TARGETS_TO_BUILD=LoongArch \
   -DLLVM_ENABLE_PROJECTS="clang;flang;lld" \
+  -DLLVM_ENABLE_RUNTIMES=compiler-rt \
   -G Ninja 
 
 ninja -v
