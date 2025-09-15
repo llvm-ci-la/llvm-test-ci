779d60ffbcefb7d5ec90e12cbc4194cff710d559
tangaac <tangyan01@loongson.cn>
2025-09-15 14:52:05 +0800

    Support lldb

--- scripts/build-llvm-release.sh
+++ scripts/build-llvm-release.sh
@@ -11,7 +11,7 @@ cmake $LLVM_SOURCE_DIR/llvm \
   -DLLVM_USE_LINKER=lld \
   -DBUILD_SHARED_LIBS=true \
   -DLLVM_TARGETS_TO_BUILD=LoongArch \
-  -DLLVM_ENABLE_PROJECTS="clang;flang;lld" \
+  -DLLVM_ENABLE_PROJECTS="clang;flang;lld;lldb" \
   -DLLVM_ENABLE_RUNTIMES=compiler-rt \
   -G Ninja 
 
